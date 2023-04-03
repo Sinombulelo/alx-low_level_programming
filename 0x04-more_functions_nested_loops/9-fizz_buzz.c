1d1636b8578f0e1134bb82e99098a188f59a4530
sinombulelo <sinombulelondeya88@gmail.com>
2023-04-03 13:18:41 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,10 @@ int main(void)
 {
 int num;
 while (num++ < 100)
-
 	if ((num % 3 == 0) && (num % 5 == 0))if ((num % 3 == 0) && (num % 5 == 0))
 		printf("FizzBuzz ");
-	
 	else if ((num % 3) == 0)
 		printf("Fizz ");
-
 	else if ((num % 5) == 0)
 	{
 		if (num != 100)
@@ -24,7 +21,6 @@ while (num++ < 100)
 		else
 			printf("Buzz");
 	}
-
 	else
 		printf("%d ", num);
 
