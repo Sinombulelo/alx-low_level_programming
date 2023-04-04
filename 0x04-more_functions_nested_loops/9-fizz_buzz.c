becfb12701f4562b3adfafd08644c252bd25b97f
sinombulelo <sinombulelondeya88@gmail.com>
2023-04-04 03:25:19 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Enty point
- * Return: 0
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-int num;
-while (num++ < 100)
-	if ((num % 3 == 0) && (num % 5 == 0))if ((num % 3 == 0) && (num % 5 == 0))
-		printf("FizzBuzz ");
-	else if ((num % 3) == 0)
-		printf("Fizz ");
-	else if ((num % 5) == 0)
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (num != 100)
-			printf("Buzz ");
-
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
 		else
-			printf("Buzz");
+		{
+			printf(" %d", n);
+		}
 	}
-	else
-		printf("%d ", num);
-
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
