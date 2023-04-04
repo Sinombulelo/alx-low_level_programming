4408ad61b4fc1e7ecb6c9ebb0fcce1f0636792d9
sinombulelo <sinombulelondeya88@gmail.com>
2023-04-04 02:57:31 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,39 +1,35 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_number - print integers
- * @n: integer value.
- *
- * Return: nothing
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
-	unsigned int a, b, count, tmp, pow;
-
-	a = n;
-	pow = b = 1;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		a *= -1;
-		_putchar('-');
+		_putchar(45);
+		m = n * -1;
 	}
-
-	tmp = a;
-
-	while (tmp > 9)
+	else
 	{
-		b++;
-		tmp /= 10;
+		m = n;
 	}
 
-	for (count = 1; count < b; count++)
-		pow *= 10;
+	d = m;
+	count = 1;
 
-	while (pow > 1)
+	while (d > 9)
 	{
-		_putchar((a / pow) % 10 + '0');
-		pow = pow / 10;
+		d /= 10;
+		count *= 10;
 	}
-	_putchar(a % 10 + '0');
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
