8eb7fc843adbfad1b20d5d88e9c3712d1ff70ddf
sinombulelo <sinombulelondeya88@gmail.com>
2023-04-04 12:45:23 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ * @s:target
+ * @b: constant byte
+ * @n:number of byte
+ * Return: returns new value of target
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
